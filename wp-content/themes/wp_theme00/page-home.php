418aa16d1291654891112d59356b700f386555d5
sanjaypj <sanjaypjayan2000@gmail.com>
2018-08-17 08:53:23 -0700

    divided into templates

--- wp-content/themes/wp_theme00/page-home.php
+++ wp-content/themes/wp_theme00/page-home.php
@@ -6,31 +6,9 @@ Template Name: Home Page
 get_header();
 ?>
 
-  <!-- conetent start here -->
-  <div class="container">
-    <!-- Example row of columns -->
-    <div class="row">
-      <?php 
-        $args = array(
-          'post_type' => 'news',
-          'orderby' => 'post_id',
-          'order' => 'ASC'
-        );
-        $loop = new WP_Query($args);
-        while ( $loop->have_posts() ) : $loop->the_post();
-      ?>
-      <div class="col-md-4">
-        <h4 class="pb-2">
-          <b><?php the_title(); ?></b>
-        </h4>
-        <p class="text-justify"><?php the_field('description'); ?></p>
-        <p>
-          <a class="btn btn-sm btn-secondary" href="#" role="button">View details »</a>
-        </p>
-      </div>
-      <?php endwhile; ?>
-    </div>
-  </div>
 
-  <?php
+<?php
+get_template_part( 'template-parts/content', 'home' );
+
 get_footer();
+?>
